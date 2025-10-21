09aa69316597acfbfc76cfec2431d2849b17d952
Marcelo Antonio <celosmg1@gmail.com>
2025-10-21 16:08:09 -0300

    corrigindo o excluir

--- acoes/views.py
+++ acoes/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views import View
 from django.urls import reverse_lazy
+from django.db import transaction
 from django.db.models import Q, Exists, OuterRef
 from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Asset, UserAsset
 from .forms import AssetForm
@@ -41,6 +43,18 @@ class AssetDeleteView(LoginRequiredMixin, DeleteView):
     model = Asset
     template_name = 'acoes/asset_confirm_delete.html'
     success_url = reverse_lazy('acoes:lista')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        with transaction.atomic():
+            self.object.quotes.all().delete()
+            self.object.missing_logs.all().delete()
+            try:
+                self.object.live_quote.delete()
+            except ObjectDoesNotExist:
+                pass
+            response = super().delete(request, *args, **kwargs)
+        return response
 
 class ToggleFavoriteView(LoginRequiredMixin, View):
     def post(self, request, pk):
